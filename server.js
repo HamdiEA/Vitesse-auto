f8ed9c7581756ca8b6c14b4ca031149224d146c5
xD4ever <boutheljayoussef1@gmail.comgit config --global user.email boutheljayoussef1@gmail.comgit config --global user.email boutheljayoussef1@gmail.com>
2025-02-25 16:38:41 +0100

    testing

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
 import express from 'express';
+
 const app = express();
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    const user_ip = 
-        req.headers['cf-connecting-ip'] ||
-        req.headers['x-real-ip'] ||
-        req.headers['x-forwarded-for'] ||
-        req.socket.remoteAddress ||'';
+    let user_ip = 
+        req.headers['cf-connecting-ip'] || 
+        req.headers['x-real-ip'] || 
+        req.headers['x-forwarded-for'] || 
+        req.socket.remoteAddress || '';
 
-    return res.json({ 
-        
-        user_ip,
+    // If the IP is IPv6-mapped IPv4 (e.g., ::ffff:192.168.1.1), extract the IPv4 part
+    if (user_ip.startsWith('::ffff:')) {
+        user_ip = user_ip.replace('::ffff:', '');
+    }
 
-     });
+    return res.json({ user_ip });
 });
 
-app.listen(3000, () => {
+// Listen on IPv4 only to avoid defaulting to IPv6 (::1)
+app.listen(3000, '0.0.0.0', () => {
     console.log('Server is running on port 3000');
 });
-
-// to recommits the server.js file
