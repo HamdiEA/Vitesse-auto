e7029433e8d6d602e01d6205030c72206c7c4e5b
xD4ever <boutheljayoussef1@gmail.comgit config --global user.email boutheljayoussef1@gmail.comgit config --global user.email boutheljayoussef1@gmail.com>
2025-02-25 15:48:04 +0100

    did the extraction of the ip adress but is extracts IPv6 not the IPv4 and working on a fix

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 import express from 'express';
-
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+app.use(express.json());
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.get('/', (req, res) => {
+    const user_ip = 
+        req.headers['cf-connecting-ip'] ||
+        req.headers['x-real-ip'] ||
+        req.headers['x-forwarded-for'] ||
+        req.socket.remoteAddress ||'';
+
+    return res.json({ 
+        
+        user_ip,
+
+     });
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
 });
