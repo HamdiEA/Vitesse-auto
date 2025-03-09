3c7876e052426cdf6614957c7b55e3ff4a3953db
xD4ever <boutheljayoussef1@gmail.comgit config --global user.email boutheljayoussef1@gmail.comgit config --global user.email boutheljayoussef1@gmail.com>
2025-03-09 22:58:22 +0100

    integrated the APIs and starting the testing

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
-document.getElementById('btn-confirmation').addEventListener('click', function(event) {
-    event.preventDefault();  // Empêche la soumission traditionnelle du formulaire
+fetch('https://api64.ipify.org?format=json')
+  .then(response => response.json())
+  .then(data => {
+    const userIP = data.ip;
+    console.log('User IP:', userIP);
+    
+    return fetch(`https://ipapi.co/${userIP}/json/`);
+  })
+  .then(response => response.json())
+  .then(jsonData => {
+    console.log('Location Data:', jsonData);
 
-    var nom = document.forms['rentalForm']['nom'].value;
-    var prenom = document.forms['rentalForm']['prenom'].value;
-    var email = document.forms['rentalForm']['email'].value;
-    var tel = document.forms['rentalForm']['tel'].value;
-    var dateDebut = document.forms['rentalForm']['date_debut'].value;
-    var duree = document.forms['rentalForm']['duree'].value;
-    var idVoiture = document.forms['rentalForm']['id_voiture'].value;
+    const currency = jsonData.currency;
+    console.log('Currency:', currency);
 
-    if (nom && prenom && email && tel && dateDebut && duree && idVoiture) {
-        alert('Merci ' + prenom + ' ' + nom + ' !\n' +
-              'Votre réservation est confirmée\n' +
-              'pour la voiture ID: ' + idVoiture);
-    } else {
-        alert('Veuillez remplir tous les champs.');
-    }
-});
-
-let prix_audi_r8 = 500;
-let prix_bmw_m4 = 700;
-let prix_bmw_M8coupé = 900;
-let prix_bmw_serie4 = 800;
-let prix_VW_gti = 600;
+    return fetch(`https://api.currencyapi.com/v3/latest?apikey=cur_live_ikQtRU8hewDkQaGUgfLs1zf1YSZubte7TIuovCll&currencies=${currency}&base_currency=EUR`);
+  })
+  .then(response => response.json())
+  .then(exchangeData => {
+    console.log('Exchange Rate Data:', exchangeData);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
