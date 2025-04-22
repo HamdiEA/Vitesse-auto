8e81f18d6d70e46e3c01e31c4aecdb62b73233a9
XD4ever <boutheljayoussef1@gmail.com>
2025-04-22 12:42:07 +0200

    Update

--- Javascript/script-form.js
+++ Javascript/script-form.js
@@ -9,6 +9,8 @@ function sendMail(){
     emailjs.send("service_kagci7q", "template_5obibh4", parms)
     .then(function(response) {
         alert("Votre message a bien été envoyé !");
+        // Since index.html is in the same folder as forum.html
+        window.location.href = "index.html";
     })
     .catch(function(error) {
         alert("Erreur lors de l'envoi du message : " + error);
