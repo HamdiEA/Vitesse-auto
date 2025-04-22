bd793494b80379580249a1a8bd5fbdc6dcc25afd
XD4ever <boutheljayoussef1@gmail.com>
2025-04-23 00:23:12 +0200

    Featue : the email sending feature is complete

--- Javascript/script-form.js
+++ Javascript/script-form.js
@@ -1,21 +1,19 @@
-function sendMail(){
+function sendMail(event){
     parms = {
-        nom : document.getElementById("nom").value,
-        email : document.getElementById("email").value,
+    nom : document.getElementById("nom").value,
+    email : document.getElementById("email").value,
     }
     
     console.log(parms);
-
+    
     emailjs.send("service_kagci7q", "template_5obibh4", parms)
     .then(
-        function(_response) {
-            console.log("SUCCESS!");
-            alert("Votre message a bien été envoyé !");
-            // Option 1: Use the root path
-            document.location = "/";
-        },
-        function(error) {
-            alert("Erreur lors de l'envoi du message : " + error);
-        }
+    function(_response) {
+    console.log("SUCCESS!");
+    alert("Votre message a bien été envoyé !");
+    window.location.href = "/html/index.html"; },
+    function(error) {
+    alert("Erreur lors de l'envoi du message : " + error);
+    }
     );
-}
+    }
