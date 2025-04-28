507efd99fb1f67fddf73c3e47248f1892d811a24
XD4ever <boutheljayoussef1@gmail.com>
2025-04-28 17:07:09 +0200

    Fix :fixed the hight of the images

--- server.js
+++ server.js
@@ -27,9 +27,9 @@ const db = mysql.createConnection({
 
 db.connect(err => {
   if (err) {
-    console.error('MySQL connection failed:', err.message);
+    console.error('❌ MySQL connection failed:', err.message);
   } else {
-    console.log('Connected to MySQL');
+    console.log('✅ Connected to MySQL');
   }
 });
 
